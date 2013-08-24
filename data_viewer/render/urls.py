1b2aef583dce66ed706acc3e14723326b37a7efe
h0pbeat <arekquest@gmail.com>
2013-08-24 18:51:17 +0000

    render urls

--- data_viewer/render/urls.py
+++ data_viewer/render/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'test', 'render.views.test', name='test'),
+	url(r'raw_phone_location', 'render.raw.location', name='raw_phone_location'),
 	url(r'^', 'render.views.home', name='home'),
 )
