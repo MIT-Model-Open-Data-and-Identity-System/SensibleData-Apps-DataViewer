09bdfef77947b985baeaf6e70c6973bb499e6c6a
cintiav <cintiavaliente@gmail.com>
2013-10-22 13:05:34 +0000

    changes added

--- data_viewer/render/utils.py
+++ data_viewer/render/utils.py
@@ -1,8 +1,8 @@
 import json
 
-def createExampleDoc(document):
+def createExampleDoc(document,question):
 	output = []
-	createExampleDocWorker(document, [], output)
+	createExampleDocWorker(document, [], output,question)
 	dd = reduce(merge, output)
 	return json.dumps(dd, sort_keys=True, indent=2, separators=(',', ':')).replace('\\"', '"')
 
@@ -13,29 +13,37 @@ def createExampleDoc(document):
 #results= anonymizeFacebookStatuses(data)
 
 
-def createExampleDocWorker(document, keys, output):
+def createExampleDocWorker(document, keys, output,question):
 	if type(document) == type([]):
 		for jj, item in enumerate(document):
-			t_keys = list(keys)
-			t_keys.append(str(jj))
-			createExampleDocWorker(item, t_keys, output)
+			createExampleDocWorker(item, keys, output,question)			
+			#t_keys = list(keys)
+			#t_keys.append(str(jj))
+			#try:
+			#	t_keys = list(keys)
+			#	t_keys.append(item.keys())			
+			#	createExampleDocWorker(item, t_keys, output)
+			#except Exception: pass
 	elif type(document) == type({}):
 		for key, value in document.items():
 			t_keys = list(keys)
 			t_keys.append(key)
-			createExampleDocWorker(value, t_keys, output)
+			createExampleDocWorker(value, t_keys, output,question)
 	else:
-		keys = processKeys(keys)
+		keys = processKeys(keys,question)
 		d = {}
 		add_keys(d, keys, document)
 		output.append(d)
 
-def processKeys(keys):
+def processKeys(keys,question=True):
 		new_keys = []
 		ignored_keys = ['0', '1']
 		for jj,key in enumerate(keys):
 			if not key in ignored_keys:
-				new_keys.append("<a href='#' onclick=\"$('#fields')[0].value += '"+ ".".join(keys[:jj]+[key])+",'\">"+str(key)+"</a>")
+				if question:
+					new_keys.append("<a href='#' onclick=\"$('#questions')[0].value += '"+ ".".join(keys[:jj]+[key])+",'\">"+str(key)+"</a>")
+				else:
+					new_keys.append("<a href='#' onclick=\"$('#fields')[0].value += '"+ ".".join(keys[:jj]+[key])+",'\">"+str(key)+"</a>")
 			else: new_keys.append(key)
 		return new_keys
 
@@ -49,7 +57,8 @@ def merge(a, b, path=None):
 			    	elif a[key] == b[key]:
 					pass # same leaf value
 			    	else:
-					raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+					pass					
+					#raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
 			else:
 			    	a[key] = b[key]
 
