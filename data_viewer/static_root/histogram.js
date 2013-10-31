271564da6c32d68623dceb56e9597c0bd5b129d5
cintiav <cintiavaliente@gmail.com>
2013-10-31 10:24:16 +0000

    last version

--- data_viewer/static_root/histogram.js
+++ data_viewer/static_root/histogram.js
@@ -22,12 +22,18 @@ function visualization(results){
 	
 	newlist=countlist
 	keysanswers=Object.keys(newlist);
+	percent=0;
 	maxval=0
 
+	if (results.length<=10){percent=10}
+	else if(results.length>10 && results.length<=100){percent=10}
+	else if(results.length>100 && results.length<=1000){percent=1000}
+
 	for (i=0;i<keysanswers.length;i++){
-		val=countlist[keysanswers[i]]/1000;
-		data.push({'letter':keysanswers[i], 'frequency':val})
-		if (val>maxval){maxval=val}
+		val=countlist[keysanswers[i]];
+		freq=val/results.length
+		data.push({'letter':keysanswers[i], 'frequency':freq})
+		if (freq>maxval){maxval=freq}
 	}
 
 	BuildData(data,keysanswers,maxval)
@@ -36,13 +42,14 @@ function visualization(results){
 
 function BuildData(data,answers,maxval){
 	document.getElementById('histogram').innerHTML = '';
-
+	document.getElementById('scatterplot').innerHTML = '';
+				
 	$('#histogram').css('display', 'block');
-	var margin = {top: 20, right: 20, bottom: 400, left: 40},
-	    width = 560 - margin.left - margin.right,
-	   height = 1000 - margin.top - margin.bottom;
+	var margin = {top: 40, right: 40, bottom: 40, left: 40},
+	    width = 75*answers.length - margin.left - margin.right,
+	   height = 500 - margin.top - margin.bottom;
 
-	var formatPercent = d3.format(".0%");
+	var formatPercent = d3.format(".00%");
 
 	var x = d3.scale.ordinal()
 	    .rangeRoundBands([0, width], .1);
@@ -65,6 +72,16 @@ function BuildData(data,answers,maxval){
 	  .append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+	var div = d3.select("#histogram").append("div")   
+	    .attr("class", "tooltip")               
+	    .style("opacity", 0);
+
+
+	ans=[];
+	for (i=0;i<answers.length;i++){
+		ans.push({'answer':answers[i]})
+	}	
+
 	
 	  x.domain(answers);
 	  y.domain([0,maxval])
@@ -74,19 +91,34 @@ function BuildData(data,answers,maxval){
 	      .attr("transform", "translate(0," + height + ")")
 	      .call(xAxis)
 	      .selectAll("text")  
+		.data(ans) 
               	.style("text-anchor", "end")
             	.attr("x", -6)
             	.attr("dx", ".50em")
             	.attr("transform", function(d) {
                 	return "rotate(-90)" 
-                });
+                }).html(function(d){
+			return d.answer.substring(0,3)
+		})
+		.on("mouseover", function(d) {      
+			    div.transition()        
+				.style("opacity", .99);      
+			    div .html(d.answer)  
+				.style("left", (d3.event.pageX) + "px")     
+				.style("top", (d3.event.pageY - 28) + "px");    
+			    })                  
+		.on("mouseout", function(d) {       
+			    div.transition()        
+				.duration(500)      
+				.style("opacity", 0);   
+			});
 
 	  svg.append("g")
 	      .attr("class", "y axis")
 	      .call(yAxis)
 	    .append("text")
-	      .attr("transform", "rotate(-90)")
-	      .attr("y", 6)
+	      .attr("y", -20)
+	      .attr("x",15)
 	      .attr("dy", ".71em")
 	      .style("text-anchor", "end")
 	      .text("Percent");
