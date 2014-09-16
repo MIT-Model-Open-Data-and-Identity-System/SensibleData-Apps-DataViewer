ab13d65ccc986013418c8a36ee855082e4a4cffe
radugatej <radu.gatej@gmail.com>
2014-09-16 17:13:46 +0200

    Raw phone data pages can now take date and users_to_return parameters for preloading the fields with values

--- data_viewer/render/raw.py
+++ data_viewer/render/raw.py
@@ -28,13 +28,21 @@ def getTokens(request):
 def build_response_phone_data(request, endpoint):
 	tokens = getTokens(request)
 	base_url = settings.SERVICE_URL+'connectors/connector_raw/v1/' + endpoint
+	users_to_return =  request.GET.get("users_to_return", "")
+	start_date = request.GET.get("start_date", "")
+	end_date = request.GET.get("end_date", "")
 
 	scope = 'connector_raw.all_data=checked'
 	example_doc = create_links(json.loads(open(settings.ROOT_DIR+'render/example_' + endpoint + '.json').read()), "fields")
 	if not tokens:
 		return get_start_auth_response(request, scope)
 
-	return render_to_response('todo_data2_trans.html', {'tokens': tokens, 'example_doc': example_doc, 'base_url': base_url, endpoint:True}, context_instance=RequestContext(request))
+	return render_to_response('todo_data2_trans.html', {'tokens': tokens,
+														'example_doc': example_doc,
+														'base_url': base_url,
+														'users_to_return': users_to_return,
+														'start_date': start_date,
+														'end_date': end_date, endpoint: True}, context_instance=RequestContext(request))
 
 def build_response_facebook(request, endpoint):
 	tokens = getTokens(request)
